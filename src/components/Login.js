9c9df4d78464c80354f00d57fe2cc014350efed5
LungileFuze <ngcobolungile94@outlook.com>
2023-10-30 10:34:29 +0200

    Designed sign in page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,20 +1,43 @@
-import React, {useState} from 'react'
-import ReactDOM from 'react-dom'
-import Modal from './Modal'
+import  React, {useState} from "react"
+import { Link } from "react-router-dom"
+import "./Login.css" 
 
 
 const Login = () => {
-
-  const [isOpen, setIsOpen] = useState(false)
   return (
-    <>
-      <h1>Login Page</h1>
-      <p>This is a login page</p>
-      <button onClick={() => setIsOpen(true)}>Open Modal</button>
-      {isOpen && ReactDOM.createPortal(<Modal setIsOpen={setIsOpen}/>,
-      document.getElementById("modal-root-div")
-     )}
-    </>
+    <div className="login">
+        <Link to="/" className="custom-links">
+            <img
+              src="https://ledgergurus.com/wp-content/uploads/2019/02/amazon-logo-transparent-800x258.png"
+              alt="amazon-logo"
+              className="login-logo"
+            />
+        </Link>
+        <div  className="login-container">
+          <h1>Sign in</h1>
+          <form>
+            <label>E-mail or mobile phone number</label>
+            <input type="text" />
+            <label>Password</label>
+            <input type="password" />
+            <button type="submit" className="signIn-button">
+              Sign in
+            </button>
+          </form>
+          <p>By continuing, you agree to Amazon's <a href="">Conditions of Use </a> and <a href="">Privacy Notice</a>.</p>
+          <p>
+          <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 320 512">
+            <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/></svg>
+            <span> <a href="#">Need help?</a></span>
+          </p>
+          <hr aria-hidden="true" className="a-divider-normal"></hr>
+          <p>Buy for work?</p>
+          <div><a href="">Shop on amazon business</a></div>
+          
+        </div>
+        <p className="separator">New to amazon?</p>
+        <button className="register-button">Create your Amazon Account</button>
+    </div>
   )
 }
 
